43f7c0071651bb7fdf48cfdc2c66e4786ab10a4b
Iñigo Huguet <inigohuguet@hotmail.com>
2020-12-12 09:07:26 +0100

    Test decrementar sellIn

--- test/gilded-rose.test.ts
+++ test/gilded-rose.test.ts
@@ -1,11 +1,17 @@
 import { Item, GildedRose } from "../app/gilded-rose";
 
-describe("Gilded Rose", function () {
-  it("Normal item must decrement its quality before sellIn date", function () {
+describe("Normal item", function () {
+  it("SellIn must decremented by one when calling updateQuality", function () {
     let normalItem = new Item("Elixir of the Mongoose", 10, 5);
     const gildedRose = new GildedRose([normalItem]);
     [normalItem] = gildedRose.updateQuality();
     expect(normalItem.sellIn).toEqual(9);
+  });
+
+  it("Must decrement its quality before sellIn date", function () {
+    let normalItem = new Item("Elixir of the Mongoose", 10, 5);
+    const gildedRose = new GildedRose([normalItem]);
+    [normalItem] = gildedRose.updateQuality();
     expect(normalItem.quality).toEqual(4);
   });
 });
