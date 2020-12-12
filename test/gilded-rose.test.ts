a36c6b1a271ff91f2f0b87ec5d3c311a7cc0ad87
Iñigo Huguet <inigohuguet@hotmail.com>
2020-12-12 08:51:58 +0100

    Test caso normal

--- test/gilded-rose.test.ts
+++ test/gilded-rose.test.ts
@@ -1,9 +1,11 @@
 import { Item, GildedRose } from "../app/gilded-rose";
 
 describe("Gilded Rose", function () {
-  it("should foo", function () {
-    const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+  it("Normal item must decrement its quality before sellIn date", function () {
+    let normalItem = new Item("Elixir of the Mongoose", 10, 5);
+    const gildedRose = new GildedRose([normalItem]);
+    [normalItem] = gildedRose.updateQuality();
+    expect(normalItem.sellIn).toEqual(9);
+    expect(normalItem.quality).toEqual(4);
   });
 });
