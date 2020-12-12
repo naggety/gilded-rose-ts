1ad550fd8c381b7803f97d540aa5b31511dafd52
Iñigo Huguet <inigohuguet@hotmail.com>
2020-12-12 09:08:00 +0100

    Test bordercase sellIn 1->0

--- test/gilded-rose.test.ts
+++ test/gilded-rose.test.ts
@@ -14,4 +14,11 @@ describe("Normal item", function () {
     [normalItem] = gildedRose.updateQuality();
     expect(normalItem.quality).toEqual(4);
   });
+
+  it("Quality must be decremented only one when sellIn updates from 1 to 0", function() {
+    let normalItem = new Item("Elixir of the Mongoose", 1, 5);
+    const gildedRose = new GildedRose([normalItem]);
+    [normalItem] = gildedRose.updateQuality();
+    expect(normalItem.quality).toEqual(4);
+  });
 });
